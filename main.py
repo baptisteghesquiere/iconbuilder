63215774544b8ed0c68a8272e0f30954befc17a9
baptiste <baptiste.ghesquiere@accomodata.be>
2021-06-23 16:34:26 +0200

    changed path to path_to_exported_png

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#!usr/bin/env python3
 # Requires svglib and cairosvg to be installed
+import subprocess
 
 from svglib.svglib import svg2rlg
 import cairosvg
@@ -12,23 +10,23 @@ import os
 import io
 
 path_to_svg_file = "svg/base_odoo_icon_text.svg"
-string_to_search = "{modulet}"
+string_to_search = "{module}"
 path_to_updated_svg = "svg/icon.svg"
-path_to_png = "svg/icon.png"
+path_to_exported_png = "svg/icon.png"
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     name = input("Geef de naam van de module:")
 
     def replacer(name):
-        f = open(path_to_svg_file,'r')
+        f = open(path_to_svg_file, "rt")
         data = f.read()
         data = data.replace(string_to_search, name)
         f.close()
         f = open(path_to_updated_svg, "wt")
         f.write(data)
         f.close()
-        cairosvg.svg2png(path_to_updated_svg, write_to=path_to_png)
+        subprocess.run(['inkscape', path_to_updated_svg, '-e', path_to_exported_png])
         print(f)
         print(str(name))
 
